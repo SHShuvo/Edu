eca5555c5de7718dc7b873b0a1c6f385ec8a0b27
sajjadshuvo <sajjadshuvo1212@gmail.com>
2022-06-28 01:11:18 +0600

    vue x added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
